cfce6fed22bfb39e9d8797e354be65a3d0feb28c
RRmalargue <rrojasroco@gmail.com>
2026-09-01 22:36:09 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // sw.js - Service Worker para habilitar la instalación de la PWA
-const CACHE_NAME = 'trail-portal-v3.7';
+const CACHE_NAME = 'trail-portal-v3.8';
 const ASSETS = [
     './index.html',
     './index.css',
