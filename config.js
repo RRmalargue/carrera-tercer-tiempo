eb172562912685c0ae21e9e680437244f369f2d3
RRmalargue <rrojasroco@gmail.com>
2026-09-02 19:31:41 -0300

    Add files via upload

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ window.RACE_CONFIG = {
   "googleScriptUrl": "",
   "raceDescription": " REGLAMENTO\n\nCarrera DE CROSS TRAIL a realizarse el 20 de setiembre de 2026 en Malargüe, lugar predio TERCER TIEMPO (LAS BARDAS SIN NUMERO BARRIO VIRGEN DE LOS VIENTOS MALARGUE, MENDOZA),con largada a las 09:00 hs.\n\nEn 2 distancias 5 km,  15 km \n\nCON LA INSCRIPCION SE ENTREGA REMERA DEL EVENTO  Cupo de 200 corredores\n\ncupo de REMERAS 200 UNIDADES.\n\nCosto de inscripción\n\n5 km: 35.000 pesos\n\n15 Km: 50.000 pesos\n\nCATEGORIA KID 18.000 pesos\n\nSOCIOS DEL CLUB TENDRAN 20% DE DESCUENTO.\n\nLa Organización podrá suspender y/o posponer el evento por cuestiones de seguridad pública, pandemia, climatológicas y/o razones de fuerza mayor.\n\nLos competidores al momento de acreditarse para retirar su número y kit deberán presentar obligatoriamente el DNI. La entrega de Kit se hará el día sábado 19 DE Setiembre en lugar a designar.\n\nPREMIACION\n\n\nPREMIACION EN EFECTIVO \n\n15 KMS PARA DAMAS Y CABALLEROS\n\n1 PUESTO     200.000 PESOS\n2 PUESTO     150.000 PESOS\n3 PUESTO     100.000 PESOS\n\n5 KM PARA DAMAS Y CABALLEROS\n\n1 PUESTO      70.000,00 PESOS\n2 PUESTO      50.000,00 PESOS\n3 PUESTO      35.000,00 PESOS\n\nMEDALLAS FINISHER PARA TODOS LOS PARTICIPANTES\n\nEN 15 KMS REGALOS EN TODAS LAS CATEGORIAS HASTA EL TERCER PUESTO\n\nEl Corredor que clasifique al podio; deberá estar presente para recibir su premio, no podrá subir otra persona en su lugar.",
   "logoImage": "./IMAGENES/LOGO.jpg",
-  "contactWhatsapp": "5492604656244",
+  "contactWhatsapp": "5492604552146",
   "posterImage": "./IMAGENES/AFICHE TERCER.jpg",
   "tshirtImage": "./IMAGENES/REMERA TERCER.jpg",
   "altitudeMapImage": "./IMAGENES/MAPADEALTURA15.jpg",
@@ -178,8 +178,8 @@ window.RACE_CONFIG = {
     "secondary": "#ff007f",
     "secondaryGlow": "rgba(255, 0, 127, 0.35)"
   },
-  "themeBackground": "sunset",
-  "borderRadiusStyle": "10px",
+  "themeBackground": "snow",
+  "borderRadiusStyle": "20px",
   "formFields": [
     {
       "id": "nombre",
